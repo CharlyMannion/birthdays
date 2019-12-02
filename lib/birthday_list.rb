31d71cc36e9ac61fb57044c61229e1c9d6b1a102
Charlotte Mannion <charlottemannion100@gmail.com>
2019-12-02 10:54:54 +0000

    writes first unit test for birthday list class

--- lib/birthday_list.rb
+++ lib/birthday_list.rb
@@ -1,2 +1,9 @@
 class Birthday_list
+
+  def add_birthday
+  end
+  
 end
+
+# irb:
+# require './lib/birthday_list.rb
--- spec/birthday_list_spec.rb
+++ spec/birthday_list_spec.rb
@@ -1,5 +1,6 @@
 require 'birthday_list'
 
 describe Birthday_list do
+  it { is_expected.to respond_to :add_birthday }
 
 end
