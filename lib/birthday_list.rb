5c53165ba29afcacd38f7d22df1453c5fdb62ddb
Charlotte Mannion <charlottemannion100@gmail.com>
2019-12-03 11:43:00 +0000

    writes test for injection of birthday class into birthday_list class, passes test

--- lib/birthday_list.rb
+++ lib/birthday_list.rb
@@ -1,21 +1,30 @@
-require 'date'
+# require 'date'
 
 class Birthday_list
 
+def initialize(birthday_class = Birthday)
   @list = []
+  @birthday_class = birthday_class
+end
 
   def add_birthday
+    birthday = @birthday_class.new
+    birthday.name
   end
 
 end
 
 
 class Birthday
-  attr_accessor 'name', 'dob'
+  attr_accessor :name, :dob
 
   def initialize(name, dob)
     @name = name
     @dob = dob
+
+    # def do_whatever
+    #   puts "doing whatever"
+    # end
   end
 
 end
@@ -28,3 +37,5 @@ end
 # p birthday_test1 = Birthday.new("Jenna", "1993-07-10")
 # p birthday_test1.name
 # p birthday_test1.date
+
+p birthday_list = Birthday_list.new
--- spec/birthday_list_spec.rb
+++ spec/birthday_list_spec.rb
@@ -3,6 +3,17 @@ require 'birthday_list'
 describe Birthday_list do
   it { is_expected.to respond_to :add_birthday }
 
+  describe '#add_birthday' do
+    it 'makes a birthday' do
+      birthday_double = double :birthday, name: "Jenna", date: "1993-07-10"
+      birthday_class_double = double :birthday_class, new: birthday_double
+
+      birthday_list = Birthday_list.new(birthday_class_double)
+
+      expect(birthday_double).to receive(:name)
+      birthday_list.add_birthday
+    end
+  end
 end
 
 describe '#initialize' do
