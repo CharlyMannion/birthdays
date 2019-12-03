12bf261fbb4a44bb935b65b84e72d751daf988c6
Charlotte Mannion <charlottemannion100@gmail.com>
2019-12-03 10:39:27 +0000

    creates an instance of birthday class with name instance variable, using TDD

--- lib/birthday_list.rb
+++ lib/birthday_list.rb
@@ -1,9 +1,20 @@
 class Birthday_list
 
+  @list = []
+
   def add_birthday
   end
-  
+
+end
+
+
+class Birthday
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
 end
 
 # irb:
-# require './lib/birthday_list.rb
+# require './lib/birthday_list.rb'
--- spec/birthday_list_spec.rb
+++ spec/birthday_list_spec.rb
@@ -4,3 +4,10 @@ describe Birthday_list do
   it { is_expected.to respond_to :add_birthday }
 
 end
+
+describe '#initialize' do
+  it 'should set the instance variable name' do
+    test_birthday = Birthday.new("Jenna")
+    expect(test_birthday.name).to eq("Jenna")
+  end
+end
